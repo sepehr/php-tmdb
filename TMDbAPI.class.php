38ba8945ef88fac0a3456f429cfcc60447044e72
Sepehr Lajevardi <me@sepehr.ws>
2010-11-06 05:12:35 +0330

    Added TMDbAPI class.

---
+++ TMDbAPI.class.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * TMDb is a PHP wrapper for TMDb API which is mainly written 
+ * to be used in Drupal modules, Also to be respectful 
+ * to the coding standards of Drupal community.
+ *
+ * @version 0.1
+ *
+ * @author Sepehr Lajevardi - me@sepehr.ws
+ * 
+ * @see http://api.themoviedb.org/2.1/
+ *
+ * @license GNU General Public License 3.0
+ */
+ 
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * TMDb API class.
+ *
+ * Provides higher level interaction with http://api.themoviedb.org/2.1/.
+ */
+class TMDbAPI {
+  
+  /**
+   * TMDb constructor who cries and calls his parent!
+   *
+   * @param $key
+   *   API key.
+   * @param $server
+   *   API server address.
+   * @param $version
+   *   API version.
+   * @param $format
+   *   API call response format.
+   * @param $language
+   *   API call response language.
+   */
+  public function __construct($key, $server = TMDb::SERVER, $version = TMDb::VERSION, $format = TMDb::JSON, $language = TMDb::LANG) {
+    parent::__construct($key, $server, $version, $format, $language);
+  }
+  
+  
+}
+
