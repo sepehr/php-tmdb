18ff89c69d503ef6c629f750624a11c85c78b4e7
Sepehr Lajevardi <me@sepehr.ws>
2010-11-06 05:15:16 +0330

    Separated header comment block.

--- TMDb.class.php
+++ TMDb.class.php
@@ -11,7 +11,9 @@
  * @see http://api.themoviedb.org/2.1/
  *
  * @license GNU General Public License 3.0
- *
+ */
+ 
+/**
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
