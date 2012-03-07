e49d8ee5bf4cc5fb3af13797cf530c4baa217814
Sepehr Lajevardi <me@sepehr.ws>
2012-03-07 08:48:39 +0330

    Initial branch commit.

---
+++ tmdb.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * TMDbPHP - PHP API Wrapper for TMDb API
+ * 
+ * TMDbPHP is a PHP wrapper for TMDb API (http://api.themoviedb.org) 
+ * which is mainly written to be used in Drupal modules, not to be
+ * considered as an external library also to be respectful to the 
+ * coding standards of Drupal community. You may find more info and
+ * docs in the project homepage at http://github.com/sepehr/tmdbphp.
+ *
+ * @version 1.x-dev
+ *
+ * @author Sepehr Lajevardi <me@sepehr.ws>
+ * @copyright Copyright (c) 2011 - Sepehr Lajevardi
+ * @license GNU General Public License 3.0 - http://www.gnu.org/licenses/gpl-3.0.txt
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
